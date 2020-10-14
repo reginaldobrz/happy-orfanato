4f228414c1ca83f64cfaa89ea4d0773f54eff5fe
reginaldo <reginaldo-aguiar@hotmail.com>
2020-10-14 19:08:37 -0300

    commit-[abstraindo em controllers]

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,40 +1,9 @@
 import express from 'express';
 import './database/connection';
-import {getRepository} from 'typeorm';
-import Orphanage from './models/Orphanage';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
 
-app.post('/orphanages', async (request, response)=>{
-
-  const{
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  } = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-const orphanage = orphanagesRepository.create({
-  name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-});
-
-await orphanagesRepository.save(orphanage);
-
-  return response.status(201).json(orphanage);
-});
-
+app.use(routes);
 app.listen(3333);
-
-//PAREI NO WORKSHOP 2 MOMENTO 59:09
